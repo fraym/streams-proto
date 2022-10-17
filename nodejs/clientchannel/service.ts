cf642a63878ebaedfaffdb722f17113846a9f15e
Jesko Plitt <jp@becklyn.com>
2022-10-17 10:56:25 +0200

    Output correct grpc server implementation for typescript

--- nodejs/clientchannel/service.ts
+++ nodejs/clientchannel/service.ts
@@ -1,7 +1,20 @@
 /* eslint-disable */
-import _m0 from "protobufjs/minimal";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import {
+  CallOptions,
+  ChannelCredentials,
+  ChannelOptions,
+  Client,
+  ClientDuplexStream,
+  ClientReadableStream,
+  ClientUnaryCall,
+  handleBidiStreamingCall,
+  handleServerStreamingCall,
+  handleUnaryCall,
+  makeGenericClientConstructor,
+  Metadata,
+  ServiceError,
+  UntypedServiceImplementation,
+} from "@grpc/grpc-js";
 import { PublishEventEnvelope } from "./event";
 import { GetEventsFromStartRequest } from "./get_events_from_start";
 import { GetStreamRequest, GetStreamResponse } from "./get_stream";
@@ -10,44 +23,75 @@ import { Response } from "./response";
 
 export const protobufPackage = "clientchannel";
 
-export interface Service {
-  Connect(request: Observable<Request>): Observable<Response>;
-  GetEventsFromStart(request: GetEventsFromStartRequest): Observable<PublishEventEnvelope>;
-  GetStream(request: GetStreamRequest): Promise<GetStreamResponse>;
+export type ServiceService = typeof ServiceService;
+export const ServiceService = {
+  connect: {
+    path: "/clientchannel.Service/Connect",
+    requestStream: true,
+    responseStream: true,
+    requestSerialize: (value: Request) => Buffer.from(Request.encode(value).finish()),
+    requestDeserialize: (value: Buffer) => Request.decode(value),
+    responseSerialize: (value: Response) => Buffer.from(Response.encode(value).finish()),
+    responseDeserialize: (value: Buffer) => Response.decode(value),
+  },
+  getEventsFromStart: {
+    path: "/clientchannel.Service/GetEventsFromStart",
+    requestStream: false,
+    responseStream: true,
+    requestSerialize: (value: GetEventsFromStartRequest) =>
+      Buffer.from(GetEventsFromStartRequest.encode(value).finish()),
+    requestDeserialize: (value: Buffer) => GetEventsFromStartRequest.decode(value),
+    responseSerialize: (value: PublishEventEnvelope) => Buffer.from(PublishEventEnvelope.encode(value).finish()),
+    responseDeserialize: (value: Buffer) => PublishEventEnvelope.decode(value),
+  },
+  getStream: {
+    path: "/clientchannel.Service/GetStream",
+    requestStream: false,
+    responseStream: false,
+    requestSerialize: (value: GetStreamRequest) => Buffer.from(GetStreamRequest.encode(value).finish()),
+    requestDeserialize: (value: Buffer) => GetStreamRequest.decode(value),
+    responseSerialize: (value: GetStreamResponse) => Buffer.from(GetStreamResponse.encode(value).finish()),
+    responseDeserialize: (value: Buffer) => GetStreamResponse.decode(value),
+  },
+} as const;
+
+export interface ServiceServer extends UntypedServiceImplementation {
+  connect: handleBidiStreamingCall<Request, Response>;
+  getEventsFromStart: handleServerStreamingCall<GetEventsFromStartRequest, PublishEventEnvelope>;
+  getStream: handleUnaryCall<GetStreamRequest, GetStreamResponse>;
 }
 
-export class ServiceClientImpl implements Service {
-  private readonly rpc: Rpc;
-  private readonly service: string;
-  constructor(rpc: Rpc, opts?: { service?: string }) {
-    this.service = opts?.service || "clientchannel.Service";
-    this.rpc = rpc;
-    this.Connect = this.Connect.bind(this);
-    this.GetEventsFromStart = this.GetEventsFromStart.bind(this);
-    this.GetStream = this.GetStream.bind(this);
-  }
-  Connect(request: Observable<Request>): Observable<Response> {
-    const data = request.pipe(map((request) => Request.encode(request).finish()));
-    const result = this.rpc.bidirectionalStreamingRequest(this.service, "Connect", data);
-    return result.pipe(map((data) => Response.decode(new _m0.Reader(data))));
-  }
-
-  GetEventsFromStart(request: GetEventsFromStartRequest): Observable<PublishEventEnvelope> {
-    const data = GetEventsFromStartRequest.encode(request).finish();
-    const result = this.rpc.serverStreamingRequest(this.service, "GetEventsFromStart", data);
-    return result.pipe(map((data) => PublishEventEnvelope.decode(new _m0.Reader(data))));
-  }
-
-  GetStream(request: GetStreamRequest): Promise<GetStreamResponse> {
-    const data = GetStreamRequest.encode(request).finish();
-    const promise = this.rpc.request(this.service, "GetStream", data);
-    return promise.then((data) => GetStreamResponse.decode(new _m0.Reader(data)));
-  }
+export interface ServiceClient extends Client {
+  connect(): ClientDuplexStream<Request, Response>;
+  connect(options: Partial<CallOptions>): ClientDuplexStream<Request, Response>;
+  connect(metadata: Metadata, options?: Partial<CallOptions>): ClientDuplexStream<Request, Response>;
+  getEventsFromStart(
+    request: GetEventsFromStartRequest,
+    options?: Partial<CallOptions>,
+  ): ClientReadableStream<PublishEventEnvelope>;
+  getEventsFromStart(
+    request: GetEventsFromStartRequest,
+    metadata?: Metadata,
+    options?: Partial<CallOptions>,
+  ): ClientReadableStream<PublishEventEnvelope>;
+  getStream(
+    request: GetStreamRequest,
+    callback: (error: ServiceError | null, response: GetStreamResponse) => void,
+  ): ClientUnaryCall;
+  getStream(
+    request: GetStreamRequest,
+    metadata: Metadata,
+    callback: (error: ServiceError | null, response: GetStreamResponse) => void,
+  ): ClientUnaryCall;
+  getStream(
+    request: GetStreamRequest,
+    metadata: Metadata,
+    options: Partial<CallOptions>,
+    callback: (error: ServiceError | null, response: GetStreamResponse) => void,
+  ): ClientUnaryCall;
 }
 
-interface Rpc {
-  request(service: string, method: string, data: Uint8Array): Promise<Uint8Array>;
-  clientStreamingRequest(service: string, method: string, data: Observable<Uint8Array>): Promise<Uint8Array>;
-  serverStreamingRequest(service: string, method: string, data: Uint8Array): Observable<Uint8Array>;
-  bidirectionalStreamingRequest(service: string, method: string, data: Observable<Uint8Array>): Observable<Uint8Array>;
-}
+export const ServiceClient = makeGenericClientConstructor(ServiceService, "clientchannel.Service") as unknown as {
+  new (address: string, credentials: ChannelCredentials, options?: Partial<ChannelOptions>): ServiceClient;
+  service: typeof ServiceService;
+};
--- nodejs/proto.sh
+++ nodejs/proto.sh
@@ -9,6 +9,7 @@ protoc \
 	--ts_proto_opt=esModuleInterop=true \
 	--ts_proto_opt=env=node \
 	--ts_proto_opt=oneof=unions \
+	--ts_proto_opt=outputServices=grpc-js \
 	clientchannel/*.proto
 
 cd nodejs
